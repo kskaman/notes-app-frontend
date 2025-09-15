23a936b3f176abe7f6eafecf1c2e140290e23a53
Kamanpreet Singh <kskaman@gmail.com>
2025-09-14 23:16:09 -0230

    added empty pages for home and archived

--- src/pages/notes/archived/page.tsx
+++ src/pages/notes/archived/page.tsx
@@ -1,5 +1,28 @@
 const ArchivedPage = () => {
-  return <div>archived page</div>;
+  const archivedNotes: { title: string }[] = [];
+
+  return (
+    <div
+      className="w-full lg:w-[242px] h-full 
+      lg:border-r lg:border-(--divider)
+      py-5 pr-4 pl-8
+      flex flex-col gap-4 overflow-y-auto"
+    >
+      <p className="text-preset-5 text-[#2b303b]">
+        All your archived notes are stored here. You can restore or delete them
+        anytime.
+      </p>
+      {/* If there are no notes, display a message  
+      else display list of notes */}
+      {archivedNotes.length === 0 ? (
+        <p className="p-2 rounded-[8px] bg-[#e0e4e4] text-preset-5">
+          No notes have been archived yet. Move notes here for safekeeping.
+        </p>
+      ) : (
+        archivedNotes.map((note) => <div key={note.title}>{note.title}</div>)
+      )}
+    </div>
+  );
 };
 
 export default ArchivedPage;
--- src/pages/notes/collection/page.tsx
+++ src/pages/notes/collection/page.tsx
@@ -1,21 +1,16 @@
 import Button from "../../../ui/Button";
 
 const AllNotesPage = () => {
-  const collections: { title: string }[] = [];
+  const notes: { title: string }[] = [];
 
   return (
     <div
-      className="w-full 
+      className="w-full lg:w-[242px] h-full 
+      border-r border-(--divider)
       py-5 pr-4 pl-8
       flex flex-col gap-4 overflow-y-auto"
     >
-      <div className="flex items-center gap-4 flex-col md:flex-row">
-        <div className="w-full max-w-[250px]">
-          {/* Button to create a new note */}
-          <Button variant="primary" height="41px" icon="+">
-            Create New Collection
-          </Button>
-        </div>
+      <div className="flex items-center gap-4">
         <div className="w-full max-w-[250px]">
           {/* Button to create a new note */}
           <Button variant="primary" height="41px" icon="+">
@@ -26,15 +21,13 @@ const AllNotesPage = () => {
 
       {/* If there are no notes, display a message  
       else display list of notes */}
-      {collections.length === 0 ? (
+      {notes.length === 0 ? (
         <p className="p-2 rounded-[8px] bg-[#e0e4e4] text-preset-5">
-          You don't have any notes yet. Start a new collection or note to
-          capture your thoughts and ideas.
+          You don't have any notes yet. Start a new note to capture your
+          thoughts and ideas.
         </p>
       ) : (
-        collections.map((collection) => (
-          <div key={collection.title}>{collection.title}</div>
-        ))
+        notes.map((note) => <div key={note.title}>{note.title}</div>)
       )}
     </div>
   );
